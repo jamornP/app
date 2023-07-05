61a8864c5157ac8748b679c47525f25f10ffb665
jamornP <100953989+jamornP@users.noreply.github.com>
2023-07-05 14:15:52 +0700

    up

---
+++ part-time-job/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Part Time Job Science@kmitl</title>
+    <?php require $_SERVER['DOCUMENT_ROOT'] ."/app/components/job/link.php"; ?>
+</head>
+<body>
+    <?php require $_SERVER['DOCUMENT_ROOT'] ."/app/components/job/navbar.php"; ?>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <?php require $_SERVER['DOCUMENT_ROOT'] ."/app/components/job/script.php"; ?>
+</body>
+</html>
